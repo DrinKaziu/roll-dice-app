8101ecfbd70caa45a25783a5cc8ea86e30a0719e
SnowSurfer <drinkaziu@gmail.com>
2019-04-17 16:23:08 -0400

    style die

--- src/Die.js
+++ src/Die.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
   render () {
     return (
-      <div className="Die">
-        <i className={`fas fa-dice-${this.props.number}`}></i>
-      </div>
+      <i className={`Die fas fa-dice-${this.props.number}`}></i>
     )
   }
 }
